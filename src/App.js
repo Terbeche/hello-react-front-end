2ce009548e96cdee3e17f2ed1df0c7ee92b87135
terbeche <termus96@gmail.com>
2022-10-19 23:59:09 +0100

    Fix components and linters

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React  from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Greeting from './Greeting';
+import { fetchMessage } from './redux/messages/messageSlice';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  // useEffect(() => dispatch(fetchMessage()), []);
+
+  useEffect(() => {
+    async function fetchRandomMessage() {
+      dispatch(fetchMessage());
+    }
+
+    fetchRandomMessage();
+  }, []);
   return (
+
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Greeting />} />
       </Routes>
     </BrowserRouter>
+
   );
-}
+};
 
 export default App;
--- src/Greeting.js
+++ src/Greeting.js
@@ -1,10 +1,9 @@
-import React  from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Greeting = () => {
-  return (
-            <div>test</div>
-  );
-}
+  const message = useSelector((state) => state.messages.greeting);
+  return (<div className="message">{message}</div>);
+};
 
 export default Greeting;
