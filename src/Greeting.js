ffcc3547c2aadbe767b1eb2d0309ec79e9088691
terbeche <termus96@gmail.com>
2022-10-19 20:37:50 +0100

    Add App and Greeting components

---
+++ src/Greeting.js
@@ -0,0 +1,10 @@
+import React  from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const Greeting = () => {
+  return (
+            <div>test</div>
+  );
+}
+
+export default Greeting;
